6eca1db6d1faa70aacafba83f06654d89e3421b2
pl_konstkri <fcbsport2015@bk.ru>
2023-12-16 03:31:54 +0300

    дописал тестики

--- SearchTest.cs
+++ SearchTest.cs
@@ -1,4 +1,8 @@
-﻿using OpenQA.Selenium;
+﻿using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using NUnit.Framework;
 
 namespace testing_lab_5_selenium
 {
@@ -58,7 +62,7 @@ namespace testing_lab_5_selenium
             Assert.That(searchBox, Is.Not.Null, "Search box not found.");
         }
 
-        [Test]
+/*        [Test]
         public void AllTestSearchFunctionality()
         {
             driver.Navigate().GoToUrl("https://miet.ru/search/");
@@ -84,6 +88,69 @@ namespace testing_lab_5_selenium
 
             IWebElement specificPerson = driver.FindElement(By.XPath("//text()[.='Кожухов Игорь Борисович']"));
             Assert.That(specificPerson, Is.Not.Null, "Specific person not found in search results.");
+        }*/
+
+        [Test]
+        public void TestSearchFunctionalityForWiki()
+        {
+            driver.Navigate().GoToUrl("https://www.wikipedia.org/");
+
+            IWebElement searchInput = driver.FindElement(By.Id("searchInput"));
+            searchInput.SendKeys("Selenium (software)");
+
+            IWebElement searchButton = driver.FindElement(By.XPath("//button[@type='submit']"));
+            searchButton.Click();
+
+            Assert.That(driver.Title, Does.Contain("Selenium (software)"));
+        }
+
+        [Test]
+        public void TestForumRegistration()
+        {
+            driver.Navigate().GoToUrl("http://example.com/forum/registration");
+
+            driver.FindElement(By.Id("username")).SendKeys("testuser");
+            driver.FindElement(By.Id("email")).SendKeys("testuser@example.com");
+            driver.FindElement(By.Id("password")).SendKeys("password123");
+            driver.FindElement(By.Id("confirmPassword")).SendKeys("password123");
+
+            driver.FindElement(By.Id("registerButton")).Click();
+
+            // Проверка успешной регистрации
+            Assert.That(driver.FindElement(By.Id("successMessage")), Is.Not.Null);
+        }
+
+        [Test]
+        public void TestVotingSystem()
+        {
+            driver.Navigate().GoToUrl("http://example.com/vote");
+
+            var voteOption = driver.FindElement(By.Id("option1"));
+            voteOption.Click();
+
+            driver.FindElement(By.Id("voteButton")).Click();
+
+            // Проверка подтверждения голосования
+            Assert.That(driver.FindElement(By.Id("voteConfirmation")), Is.Not.Null);
+        }
+
+        [Test]
+        public void TestPhoneDirectorySearch()
+        {
+            driver.Navigate().GoToUrl("https://telefonio.ru/1-e_krasnikovo.html");
+
+            driver.FindElement(By.Name("fam")).SendKeys("Малахов");
+            //Thread.Sleep(4000);
+            driver.FindElement(By.Name("tel")).SendKeys("+7 (967) 662-79-98");
+            driver.FindElement(By.Id("searchButton")).Click();
+
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(3000));
+
+            var messageDiv = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id("message")));
+            var messageText = messageDiv.Text;
+
+            // Проверка наличия результатов
+            Assert.Equals("Данные найдены. Информация придет в уведомлении (не забудьте нажать кнопку разрешить).", messageText);
         }
     }
 }
